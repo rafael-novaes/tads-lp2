90fdd19db647449ba1e606fa0c73f9aa4f95faca
Ronaldo Nogueira de Sousa <ronogue.rn@gmail.com>
2026-09-03 21:55:00 -0300

    aula-0309

--- src/main/java/br/edu/ifsp/biblioteca/domain/Livro.java
+++ src/main/java/br/edu/ifsp/biblioteca/domain/Livro.java
@@ -1,16 +1,23 @@
 package br.edu.ifsp.biblioteca.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Livro {
 
     private Long id;
     private String isbn;
     private String titulo;
     private Integer anoPublicacao;
+    private List<Exemplar> listaDeExemplares;
+    private List<Autor> autores;
 
     public Livro(String isbn, String titulo, Integer anoPublicacao) {
         this.isbn = isbn;
         this.titulo = titulo;
         this.anoPublicacao = anoPublicacao;
+        this.listaDeExemplares = new ArrayList<>();
+        this.autores = new ArrayList<>();
     }
 
     public Long getId() {
@@ -45,13 +52,21 @@ public class Livro {
         this.anoPublicacao = anoPublicacao;
     }
 
-//    @Override
-//    public String toString() {
-//        return "Livro{" +
-//                "id=" + id +
-//                ", isbn='" + isbn + '\'' +
-//                ", titulo='" + titulo + '\'' +
-//                ", anoPublicacao=" + anoPublicacao +
-//                '}';
-//    }
+    public void adicionarExemplar(Exemplar item) {
+        this.listaDeExemplares.add(item);
+    }
+
+    public void adicionarAutor(Autor autor) {
+        this.autores.add(autor);
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
 }
